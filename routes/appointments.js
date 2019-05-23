477b1504926360441c5bd5e0d765873a4bb3277b
Tezza1 <tshenker@gmail.com>
2019-05-23 15:11:11 +0900

    added routing for appts

---
+++ routes/appointments.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = require('router');
+
+// ---------- MAKING AN APPOINTMENT ----------
+
+// @route     GET
+// @desc      Get form to add an appointment
+// @access    Public
+router.get('/add', (req, res) => {
+    res.send('get --> add');
+});
+
+// @route     POST
+// @desc      Add an appointment
+// @access    Public
+router.post('/add', (req, res) => {
+    res.send('post --> add');
+});
+
+
+// ---------- ADMIN LOOKING AT APPOINTMENTS ----------
+
+// @route     GET
+// @desc      Add an appointment
+// @access    Private
+router.get('/dashboard', (req, res) => {
+    res.send('get --> dashboard');
+});
+
+// @route     GET
+// @desc      Edit an appointment
+// @access    Private
+router.post('/edit/:id', (req, res) => {
+    res.send('/:edit/:id');
+});
+
+// @route     GET
+// @desc      Delete an appointment
+// @access    Private
+router.post('/delete/:id', (req, res) => {
+    res.send('/delete/:id');
+})
+
+module.export = router;
